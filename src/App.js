204ae124dda0208c9f30c6d3e8ffd801035e1ca1
Kunta-M <mariia.kunta@gmail.com>
2021-08-10 19:16:12 +0300

    lesson1_cw

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@
 // Вивести всі об'єкти з масиву
 
 import './App.css';
-import {Users} from "./lesson1_cw/Users_cw/users_cw";
+import {User} from "./lesson1_cw/components/Users";
 
 let usersList = [
     {
@@ -243,15 +243,146 @@ let usersList = [
 
 function App() {
   return (
-    <div className="User">
-        <div className="UserInfo">
-            <Users
-                id={1}
-                name={'Leanne Graham'}
-                username={'Bret'}
-                email={'Sincere@april.biz'}
-            />
-        </div>
+    <div className="Users">
+        <User
+            id={1}
+            name={'Leanne Graham'}
+            username={'Bret'}
+            email={'Sincere@april.biz'}
+            phone={'1-770-736-8031 x56442'}
+            street={'Kulas Light'}
+            suite={'Apt. 556'}
+            city={'Gwenborough'}
+            zipcode={'92998-3874'}
+            lat={'-37.3159'}
+            lng={'81.1496'}
+        />
+
+        <User
+            id={2}
+            name={'Ervin Howell'}
+            username={'Antonette'}
+            email={'Shanna@melissa.tv'}
+            phone={'010-692-6593 x09125'}
+            street={'Victor Plains'}
+            suite={'Suite 879'}
+            city={'Wisokyburgh'}
+            zipcode={'90566-7771'}
+            lat={'-43.9509'}
+            lng={'-34.4618'}
+        />
+
+        <User
+            id={3}
+            name={'Clementine Bauch'}
+            username={'Samantha'}
+            email={'Nathan@yesenia.net'}
+            phone={'1-463-123-4447'}
+            street={'Douglas Extension'}
+            suite={'Suite 847'}
+            city={'McKenziehaven'}
+            zipcode={'59590-4157'}
+            lat={'-68.6102'}
+            lng={'-47.0653'}
+        />
+
+        <User
+            id={4}
+            name={'Patricia Lebsack'}
+            username={'Karianne'}
+            email={'Julianne.OConner@kory.org'}
+            phone={'493-170-9623 x156'}
+            street={'Hoeger Mall'}
+            suite={'Apt. 692'}
+            city={'South Elvis'}
+            zipcode={'53919-4257'}
+            lat={'29.4572'}
+            lng={'-164.2990'}
+        />
+
+        <User
+            id={5}
+            name={'Chelsey Dietrich'}
+            username={'Kamren'}
+            email={'Lucio_Hettinger@annie.ca'}
+            phone={'(254)954-1289'}
+            street={'Skiles Walks'}
+            suite={'Suite 351'}
+            city={'Roscoeview'}
+            zipcode={'33263'}
+            lat={'-31.8129'}
+            lng={'62.5342'}
+        />
+
+        <User
+            id={6}
+            name={'Mrs. Dennis Schulist'}
+            username={'Leopoldo_Corkery'}
+            email={'Karley_Dach@jasper.info'}
+            phone={'1-477-935-8478 x6430'}
+            street={'Norberto Crossing'}
+            suite={'Apt. 950'}
+            city={'South Christy'}
+            zipcode={'23505-1337'}
+            lat={'-71.4197'}
+            lng={'71.7478'}
+        />
+
+        <User
+            id={7}
+            name={'Kurtis Weissnat'}
+            username={'Elwyn.Skiles'}
+            email={'Telly.Hoeger@billy.biz'}
+            phone={'210.067.6132'}
+            street={'Rex Trail'}
+            suite={'Suite 280'}
+            city={'Howemouth'}
+            zipcode={'58804-1099'}
+            lat={'24.8918'}
+            lng={'21.8984'}
+        />
+
+        <User
+            id={8}
+            name={'Nicholas Runolfsdottir V'}
+            username={'Maxime_Nienow'}
+            email={'Sherwood@rosamond.me'}
+            phone={'586.493.6943 x140'}
+            street={'Ellsworth Summit'}
+            suite={'Suite 729'}
+            city={'Aliyaview'}
+            zipcode={'45169'}
+            lat={'-14.3990'}
+            lng={'-120.7677'}
+        />
+
+        <User
+            id={9}
+            name={'Glenna Reichert'}
+            username={'Delphine'}
+            email={'Chaim_McDermott@dana.io'}
+            phone={'(775)976-6794 x41206'}
+            street={'Dayna Park'}
+            suite={'Suite 449'}
+            city={'Bartholomebury'}
+            zipcode={'76495-3109'}
+            lat={'24.6463'}
+            lng={'-168.8889'}
+        />
+
+        <User
+            id={10}
+            name={'Clementina DuBuque'}
+            username={'Moriah.Stanton'}
+            email={'Rey.Padberg@karina.biz'}
+            phone={'024-648-3804'}
+            street={'Kattie Turnpike'}
+            suite={'Suite 198'}
+            city={'Lebsackbury'}
+            zipcode={'31428-2261'}
+            lat={'-38.2386'}
+            lng={'57.2232'}
+        />
     </div>
   );
 }
