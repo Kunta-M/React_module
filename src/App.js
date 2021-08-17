01e452cb0254154383f241a748960c7082097c99
Kunta-M <mariia.kunta@gmail.com>
2021-08-17 23:16:26 +0300

    clean master

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-import Form from "./components/Form";
-import Cars from "./components/cars/Cars";
 
 function App() {
 
   return (
     <div>
-      <Form/>
-      <Cars/>
+
     </div>
   );
 }
--- src/components/car/Car.js
+++
@@ -1,7 +0,0 @@
-export default function Car({item}) {
-  return (
-    <div>
-      {item.id}. Model - {item.model} Price - {item.price} Year - {item.year}
-    </div>
-  );
-}
