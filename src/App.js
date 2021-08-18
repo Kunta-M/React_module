11ee67fffbcac240ec1ed5d88c93d417b26adf48
Kunta-M <mariia.kunta@gmail.com>
2021-08-18 18:29:12 +0300

    clean master

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-import CarsForm from "./components/cars/CarsForm";
-import Cars from "./components/cars/Cars";
 
 function App() {
 
   return (
     <div>
-      <CarsForm/>
-      <Cars/>
     </div>
   );
 }
