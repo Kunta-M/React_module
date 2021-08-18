7fd34701ef14ad8f352c8bef18516be7f29b23f3
Kunta-M <mariia.kunta@gmail.com>
2021-08-18 17:06:25 +0300

    lesson4_cw

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import CarsForm from "./components/CarsForm";
+import Cars from "./components/Cars";
 
 function App() {
 
   return (
     <div>
-
+      <CarsForm/>
+      <Cars/>
     </div>
   );
 }
