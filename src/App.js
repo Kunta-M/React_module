5fbac9a98ae24d0a95ae4e37c90a026c3dfeb7aa
Kunta-M <mariia.kunta@gmail.com>
2021-08-20 13:37:26 +0300

    clean master

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
 import './App.css'
-import{
-BrowserRouter as Router,
-Switch,
-Route,
-Link,
-withRouter
-} from "react-router-dom";
-import Cars from "./components/Cars";
-import CreateCarForm from "./components/CreateCarForm";
-import UpdateCarForm from "./components/UpdateCarForm";
 
 function App() {
 
   return (
-      <Router>
+
         <div>
-            <div className={'wrap'}>
-                <Link className={'default'} to={'/'}>to default page</Link>
-                <Link to={'/cars'}>to cars</Link>
-                <Link to={'/create-car'}>to create car</Link>
-                <Link to={'/update-car'}>to update car</Link>
-            </div>
-            <Route path={'/cars'} render={()=>{return <Cars/>}}/>
-
-            <Route path={'/create-car'} render={()=>{return <CreateCarForm/>}}/>
-
-            <Route path={'/update-car'} render={()=>{return <UpdateCarForm/>}}/>
 
         </div>
-      </Router>
+
   );
 }
 
--- src/components/Car.js
+++
@@ -1,9 +0,0 @@
-export default function Car({item}) {
-  return (
-    <div>
-      <h4>
-        {item.id}. Model - {item.model} Price - {item.price} Year - {item.year}
-      </h4>
-    </div>
-  );
-}
