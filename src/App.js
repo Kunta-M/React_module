0e7c92c16e35377a9c8af6fa803078ed561d6c94
Kunta-M <mariia.kunta@gmail.com>
2021-08-25 23:36:10 +0300

    clean master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-// є об'єкт, значення якого 0
-// Стоврити 2 кнопки.
-// Перша збільшує   значення на 10
-// Друга зменшує на 2.
-//
-// Зміну стану та виведення реалізувати через redcer
-
 import './App.css'
-import {useReducer} from "react";
-import reducer from "./reducers/reducer";
 
 export default function App() {
-
-    let [{value}, dispatch] = useReducer (reducer, {value: 0});
-
   return (
     <div>
-        <h3>state 1 - {value}</h3>
-        <button onClick={() => dispatch({obj: '1', action: '+'})}>add 10</button>
-        <button onClick={() => dispatch({obj: '1', action: '-'})}>minus 2</button>
+    App
+
     </div>
   );
 }
