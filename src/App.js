fcef4ea92d9bab6b559ab26e73cc133915296f53
Kunta-M <mariia.kunta@gmail.com>
2021-08-25 23:33:43 +0300

    lesson7_hw

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import './App.css'
+// є об'єкт, значення якого 0
+// Стоврити 2 кнопки.
+// Перша збільшує   значення на 10
+// Друга зменшує на 2.
+//
+// Зміну стану та виведення реалізувати через redcer
 
-function App() {
+import './App.css'
+import {useReducer} from "react";
+import reducer from "./reducers/reducer";
+
+export default function App() {
+
+    let [{value}, dispatch] = useReducer (reducer, {value: 0});
 
   return (
-        <div>
-
-        </div>
+    <div>
+        <h3>state 1 - {value}</h3>
+        <button onClick={() => dispatch({obj: '1', action: '+'})}>add 10</button>
+        <button onClick={() => dispatch({obj: '1', action: '-'})}>minus 2</button>
+    </div>
   );
 }
-
-export default App;
