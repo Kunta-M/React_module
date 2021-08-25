b718c616afac397a0fed4e7d75e3192c679ab2be
Kunta-M <mariia.kunta@gmail.com>
2021-08-25 18:48:38 +0300

    clean master

--- src/App.js
+++ src/App.js
@@ -1,43 +1,11 @@
-// Є дві лінки
-// /users
-// /posts
-//
-// /users - робить все те, що було на занятті останнім способом (через кнопку та ддатковий запит на сервер)
-//
-// /posts
-// Отримує posts з jsonplaceholder, виводить їх всі.
-//     Біля кожного поста зробити лінку (не кнопку) яка буде вести на детальну інформацію поста. Детальну інформацію
-// отримувати через history.state
-
 import './App.css'
-import{
-BrowserRouter as Router,
-Route,
-Link
-} from "react-router-dom";
-import Users from "./components/Users";
-import Posts from "./components/Posts";
 
 function App() {
 
   return (
-      <Router>
-
         <div>
 
-            <div className={'wrap'}>
-            <Link to={'/users'}>to users</Link>
-            <Link to={'/posts'}>to posts</Link>
-            </div>
-
-            <div>
-                <Route path={'/users'} component={Users}/>
-
-                <Route path={'/posts'} component={Posts}/>
-            </div>
-
         </div>
-      </Router>
   );
 }
 
