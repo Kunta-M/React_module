fcea22ec559bb76fbc8cb69a3f70507d906725e3
Kunta-M <mariia.kunta@gmail.com>
2021-08-26 18:04:52 +0300

    lesson7_cw

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+// стоврити об'єкт {a:0,b:0,c:0}
+// Стоврити 6 кнопок.
+//     по дві на кожен з параметрів об'єкту.
+// одна кнопка збільшує значення , інша зменшує
+// логіку реалізувати через reducer
+
 import './App.css'
+import {useReducer} from "react";
+import reducer from "./reducers/reducer";
 
 export default function App() {
+
+  let [{a, b, c}, dispatch] = useReducer(reducer, {a: 0, b: 0, c: 0});
+
   return (
     <div>
-    App
+      <h3>{a}</h3>
+      <button onClick={() =>{dispatch({obj:1, action: '+'})}}>+</button>
+      <button onClick={() =>{dispatch({obj:1, action: '-'})}}>-</button>
+
+      <h3>{b}</h3>
+      <button onClick={() =>{dispatch({obj:2, action: '+'})}}>+</button>
+      <button onClick={() =>{dispatch({obj:2, action: '-'})}}>-</button>
+
+      <h3>{c}</h3>
+      <button onClick={() =>{dispatch({obj:3, action: +10})}}>+</button>
+      <button onClick={() =>{dispatch({obj:3, action: -4})}}>-</button>
 
     </div>
   );
