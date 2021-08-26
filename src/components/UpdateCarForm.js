8ad43fd3395d246ccc1cb1bbb4e3355f895bf16b
Kunta-M <mariia.kunta@gmail.com>
2021-08-26 18:38:11 +0300

    lesson5!_cw

--- src/components/UpdateCarForm.js
+++ src/components/UpdateCarForm.js
@@ -1,49 +1,42 @@
-import Cars from "./Cars";
-import Car from "./Car";
 import {useEffect, useState} from "react";
 import {createCars, getCars, updateCars} from "../services/car.api.service";
 
 export default function UpdateCarForm() {
 
     const [cars, setCars] = useState([]);
-    const [chosenCar, setChosenCar] = useState({model:'', price:'', year:''});
-
-    useEffect(() =>{
+    const [chosenCar, setChosenCar] = useState({model: '', price: '', year: ''});
+    console.log(chosenCar);
+    useEffect(() => {
         getCars().then(value => setCars([...value]))
-    }, [cars]);
+    }, []);
 
-    const onSelectCar = (e) =>{
-        const selectCar = cars.find(car => car.id === e.target.value)
+    const onSelectCar = (e) => {
+        const selectCar = cars.find(car => car.id === +e.target.value)
         setChosenCar(selectCar)
     }
 
-    const updateChosenCar = () =>{
-        chosenCar.id
-        ? updateCars(chosenCar)
-        : createCars(chosenCar)
-    }
+    const updateChosenCar = ({target: {value, name}}) => setChosenCar({...chosenCar, [name]: value})
 
-  return (
-    <div>
-        <form action="">
-            <select onSubmit={onSelectCar}>
+    return (
+        <div>
+
+            <select onChange={onSelectCar}>
                 {
                     cars.map(item => <option
-                                            value={item.id}
-                                            key={item.id}>
-                                            {item.model} - {item.price} - {item.year}
-                                     </option>)
+                        value={item.id}
+                        key={item.id}>
+                        {item.model} - {item.price} - {item.year}
+                    </option>)
                 }
             </select>
-        </form>
 
-        <form className={'update-form'}>
-            <input type="text" name={'model'} onChange={updateChosenCar}/>
-            <input type="text" name={'price'} onChange={updateChosenCar}/>
-            <input type="number" name={'year'} onChange={updateChosenCar}/>
+            <form className={'update-form'}>
+                <input type="text" name={'model'} onChange={updateChosenCar} value={chosenCar.model}/>
+                <input type="text" name={'price'} onChange={updateChosenCar} value={chosenCar.price}/>
+                <input type="number" name={'year'} onChange={updateChosenCar} value={chosenCar.year}/>
 
-            <input type="submit" value={'Update'}/>
-        </form>
-    </div>
-  );
+                <input type="submit" value={'Update'}/>
+            </form>
+        </div>
+    );
 }
