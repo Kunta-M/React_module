8d77dcfa5840230201004e58138a1ad5d8fd2bfb
Kunta-M <mariia.kunta@gmail.com>
2021-08-26 18:26:03 +0300

    lesson7_cw_praxis

--- src/App.js
+++ src/App.js
@@ -5,27 +5,13 @@
 // логіку реалізувати через reducer
 
 import './App.css'
-import {useReducer} from "react";
-import reducer from "./reducers/reducer";
+import Users from "./components/Users";
 
 export default function App() {
 
-  let [{a, b, c}, dispatch] = useReducer(reducer, {a: 0, b: 0, c: 0});
-
   return (
     <div>
-      <h3>{a}</h3>
-      <button onClick={() =>{dispatch({obj:1, action: '+'})}}>+</button>
-      <button onClick={() =>{dispatch({obj:1, action: '-'})}}>-</button>
-
-      <h3>{b}</h3>
-      <button onClick={() =>{dispatch({obj:2, action: '+'})}}>+</button>
-      <button onClick={() =>{dispatch({obj:2, action: '-'})}}>-</button>
-
-      <h3>{c}</h3>
-      <button onClick={() =>{dispatch({obj:3, action: +10})}}>+</button>
-      <button onClick={() =>{dispatch({obj:3, action: -4})}}>-</button>
-
+      <Users/>
     </div>
   );
 }
