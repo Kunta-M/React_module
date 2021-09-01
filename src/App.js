8d90ae68b1cf98eaa60a63260426f9f9277acd8e
Kunta-M <mariia.kunta@gmail.com>
2021-09-01 19:23:41 +0300

    lesson8_cw

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+// Створити todo використовуючи Redux.
+// Має бути контрольована форма з полями: title, description(по бажанню також можете зробити через Redux).
+//
+// Що має робити наша todo?
+// 1. Додавати саме todo
+// 2. Видаляти todo
+// 3. Редагувати todo
+
 import './App.css'
+import {useDispatch, useSelector} from "react-redux";
 
 export default function App() {
 
+    let state = useSelector(state => state);
+    let dispatch = useDispatch();
+    state = {text: ""}
+
+    let onSubmit = (e) =>{
+        e.preventDefault();
+
+    }
+
     return (
         <div>
+            <h2>Todos list</h2>
+            <form onSubmit={onSubmit}>
+                <input type="text" name={'title'}/>
+                <input type="textarea"/>
+                <button>Save</button>
+                <hr/>
+                <button>Delete all</button>
+            </form>
 
         </div>
     );
