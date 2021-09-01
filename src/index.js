0be67c9e28528d270388e492e28898e3d8dac659
Kunta-M <mariia.kunta@gmail.com>
2021-09-01 19:25:13 +0300

    clean master

--- src/index.js
+++ src/index.js
@@ -3,36 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Provider} from "react-redux";
-import {createStore} from "redux";
-
-let initialState = {
-    todo: [],
-    text: "",
-}
-
-const addTodo = (state = initialState, action) =>{
-    switch (action.type){
-        case "ADD_TODO":
-            return {...state,  todo: state.todo.concat(action.payload)};
-        case "DELETE_TODO":
-            return {...state,
-                todo: state.todo.filter((todo, i) => i !== action.payload)};
-        case "UPDATE_TODO":
-            return {...state,
-                text: state.todo [action.payload]};
-        default:
-            return state;
-    }
-}
-
-const store = createStore(addTodo);
 
 ReactDOM.render(
   <React.StrictMode>
-      <Provider store={store}>
     <App />
-      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
